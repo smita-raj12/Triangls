2cc280874816b66e42dcc3c9ef3b087cc18cf83b
smita Raj <smita.raj12@gmail.com>
2021-06-03 13:28:15 -0700

    trying to add two sides

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
+function add(side1, side2) {
+  return side1 + side2;
+}
+
 $(document).ready(function() {
   $("#formtri").submit(function(event) {
     const side1 = parseInt($("input#side1").val());
     const side2 = parseInt($("input#side2").val());
     const side3 = parseInt($("input#side3").val());
     //let sum = side1+side2+side3;
-    let tsum= side1+side2
-   
-    if(side1===side2===side3){
+    const result = add(side1, side2);
+    
+    if(side1===side2 && side2===side3 && side3===side1){
       console.log("side")
       $("#Equilateral").show()
-    }else {if(side1===side2 || side2===side3 || side3===side1){
+    } else if(side1===side2 || side2===side3 || side3!==side1){
           $("#qIsosceles").show()
-      }else if(!sum){
+    }else if(result!==side3){
+
         $("#Scalene").show()
-      }else if(tsum <= side3){
+      }else if(result <= side3){
         $("NOT-a-triangle").show()
      }
-    }
     
-  
-    event.preventDefault();
+      event.preventDefault();
   });
 });
